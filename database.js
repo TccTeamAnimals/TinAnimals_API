a89faca2c9cfc882af9e7a1b1579c0d579259eb7
Rodrigo Fantibon <rodrigo.fantibon@hotmail.com>
2023-11-17 20:24:29 -0300

    ajustes

--- database.js
+++ database.js
@@ -2,8 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || 'localhost',
   dialect: 'mysql',
+  port: 3306,
 });
 
 module.exports = sequelize;
